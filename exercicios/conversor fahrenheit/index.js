46c85b7fa7a8b2d2de7a879536eabac6b4d737ab
Lucas Oliveira <104728548+Luc4ssilva@users.noreply.github.com>
2022-05-04 19:27:19 -0300

    conversor fahrenheit to celsius

---
+++ exercicios/conversor fahrenheit/index.js
@@ -0,0 +1,8 @@
+
+function clicou(){
+    var valor = document.querySelector('input#txtfar');
+    var valor1 = Number(valor.value);
+    var res = document.querySelector('div#res');
+    var resultado = (5 / 9) * (valor1 - 32);
+    res.innerHTML = `A temperatura é de <strong>${resultado.toFixed(1)}</strong> graus celsius`;
+}
