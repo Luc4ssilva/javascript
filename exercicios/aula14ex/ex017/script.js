f7a55cbf715da636f46a1713769330583159dbac
Lucas Oliveira <104728548+Luc4ssilva@users.noreply.github.com>
2022-05-06 21:27:34 -0300

    dd

--- exercicios/aula14ex/ex017/script.js
+++ exercicios/aula14ex/ex017/script.js
@@ -1,1 +1,14 @@
-window.alert('oláa')
+function tabuada(){
+    let num = document.querySelector('input#txtn');
+    let tab = document.getElementById('seltab');
+    tab.innerHTML = ''
+    if(num.value.length == 0){
+        alert('Por favor, digite um número!');
+    }else{
+        let n = Number(num.value);
+        for(let c = 1; c <= 10; c++){
+             tab.innerHTML += `<option>${n} x ${c} = ${n*c}</option>`
+        }
+    }
+    
+}
